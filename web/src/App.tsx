71f2fdf2a7b18f289ac32b3c3637a2a611811d3c
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-10-27 15:43:38 -0300

    can now call the api

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Routes from "./routes";
+import "./global.scss";
 
 const App = () => {
   return <Routes />;
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,11 +1,23 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "../../services/api";
 import "./styles.scss";
 
 const Home: React.FC = () => {
+  const [buildings, setBuildings] = useState({});
+
+  useEffect(() => {
+    async function loadBuildings() {
+      const { data } = await api.get("/imoveis");
+      setBuildings(data);
+      console.log(data);
+    }
+
+    loadBuildings();
+  }, []);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <h1 className="title">Desafio AM Fernades</h1>
     </div>
   );
 };
